fb02a5972c958f2fca29aa7a4b77cbb9d08316e4
Terry Kim <terrykim025@gmail.com>
2021-04-07 11:53:29 +0900

    Lay out HTML/CSS, get Pexels API key

---
+++ app.js
@@ -0,0 +1,8 @@
+// PEXELS API KEY
+// 563492ad6f917000010000018c7452dc64df4df08df2d1c422d637d7
+
+const auth = "563492ad6f917000010000018c7452dc64df4df08df2d1c422d637d7";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitBtn = document.querySelector(".submit-btn");
+let searchValue;
