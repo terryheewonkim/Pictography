7a1b56375b14e6d0fe42514161fd3c334509f706
Terry Kim <terrykim025@gmail.com>
2021-04-07 14:34:26 +0900

    Fetch curated photos & search photo functionalities

--- app.js
+++ app.js
@@ -4,5 +4,60 @@
 const auth = "563492ad6f917000010000018c7452dc64df4df08df2d1c422d637d7";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitBtn = document.querySelector(".submit-btn");
+const form = document.querySelector(".search-form");
 let searchValue;
+
+// EVENT LISTENERS
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  // prevent default behavior of page refreshing when form submitted
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+// Update gallery based on input
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+
+// Fetch API with url
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: auth,
+    },
+  });
+  const data = await dataFetch.json();
+  return data;
+}
+
+// Generate HTML for gallery pictures
+function generatePictures(data) {
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large} />
+    <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+// Get curated photos from Pexels
+async function curatedPhotos() {
+  const data = await fetchApi(
+    "https://api.pexels.com/v1/curated?per_page=15&page=1"
+  );
+  generatePictures(data);
+}
+
+// Search photos with query string
+async function searchPhotos(query) {
+  const data = await fetchApi(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`
+  );
+  generatePictures(data);
+}
+
+curatedPhotos();
